98e144dba83aec86002454f9a9b889f61c3c4b6f
张卓 <contrary.zhuo@gmail.com>
2015-06-18 15:01:29 +0800

    add right-click menu

---
+++ app/js/rightClickMenu.js
@@ -0,0 +1,40 @@
+ app.   
+
+    function Menu() {
+
+      var menu = new gui.Menu()
+
+      , cut = new gui.MenuItem({
+        label: "剪切"
+        , click: function() {
+          document.execCommand("cut");
+        }
+      })
+
+      , copy = new gui.MenuItem({
+        label: "复制"
+        , click: function() {
+          document.execCommand("copy");
+        }
+      })
+
+      , paste = new gui.MenuItem({
+        label: "粘贴"
+        , click: function() {
+          document.execCommand("paste");
+        }
+      })
+    ;
+
+    menu.append(cut);
+    menu.append(copy);
+    menu.append(paste);
+
+    return menu;
+  }
+
+  var menu = new Menu();
+  $(document).on("contextmenu", function(e) {
+    e.preventDefault();
+    menu.popup(e.originalEvent.x, e.originalEvent.y);
+  });
