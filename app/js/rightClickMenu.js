2dc5896400aa7e8db5c031d69293b75063fe72c5
张卓 <contrary.zhuo@gmail.com>
2015-06-18 15:03:29 +0800

    fixed

--- app/js/rightClickMenu.js
+++ app/js/rightClickMenu.js
@@ -1,40 +1,37 @@
- app.   
+function Menu() {
 
-    function Menu() {
+  var menu = new gui.Menu()
 
-      var menu = new gui.Menu()
+  , cut = new gui.MenuItem({
+    label: "剪切",
+    click: function() {
+      document.execCommand("cut");
+    }
+  })
 
-      , cut = new gui.MenuItem({
-        label: "剪切"
-        , click: function() {
-          document.execCommand("cut");
-        }
-      })
+  , copy = new gui.MenuItem({
+    label: "复制",
+    click: function() {
+      document.execCommand("copy");
+    }
+  })
 
-      , copy = new gui.MenuItem({
-        label: "复制"
-        , click: function() {
-          document.execCommand("copy");
-        }
-      })
-
-      , paste = new gui.MenuItem({
-        label: "粘贴"
-        , click: function() {
-          document.execCommand("paste");
-        }
-      })
-    ;
-
-    menu.append(cut);
-    menu.append(copy);
-    menu.append(paste);
-
-    return menu;
-  }
-
-  var menu = new Menu();
-  $(document).on("contextmenu", function(e) {
-    e.preventDefault();
-    menu.popup(e.originalEvent.x, e.originalEvent.y);
+  , paste = new gui.MenuItem({
+    label: "粘贴",
+    click: function() {
+      document.execCommand("paste");
+    }
   });
+
+  menu.append(cut);
+  menu.append(copy);
+  menu.append(paste);
+
+  return menu;
+}
+
+var menu = new Menu();
+$(document).on("contextmenu", function(e) {
+  e.preventDefault();
+  menu.popup(e.originalEvent.x, e.originalEvent.y);
+});
